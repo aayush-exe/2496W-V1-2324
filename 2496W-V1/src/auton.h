7983f8374c39c07e579e521c4c6557631424a7b9
rohanH66 <rohan.hablani@gmail.com>
2023-12-18 19:50:23 -0800

    COOK
    #cook

--- 2496W-V1/src/auton.h
+++ 2496W-V1/src/auton.h
@@ -98,106 +98,102 @@ void left_diagonal(){
 
 void rush_right_block(){ // trigger half cata -- editing this one 
     intake.move(-127);
-    drive(200, 400);
+    drive(200, 300);
     intake.move(0);
-    drive(-1600);
-    
-    turn_to(-45);
+    drive(-1600, 950);
+
+    turn_to(-37, 300);
     hangP.set(true);
-    drive(-685);
-    turn_to(-60);
-    drive(-100, 100);
-    hangP.set(false);
-    LwingsP.set(true);
-    turn_to(-90);
-    drive(-500);
-    LwingsP.set(false);
-    turn_to(90);
-    intake.move(127);
-    drive(100, 100);
-    turn_to(-90);
-    drive(-750, 1500, 5);
-    intake.move(-127);
+    delay(100);
+    drive(-1000, 900, 1, 75);
     
-    drive(600);
+    turn_to(-90, 700, 1, 80);
+    hangP.set(false);
+    RwingsP.set(true);
+    drive(-700, 200);
+    
+    RwingsP.set(false);
+    turn_to(90, 600);
+    
+    intake.move(127);
+    drive(-100, 200);
+    
+    turn_to(-70);
+    RwingsP.set(true);
+    LwingsP.set(true);
+    drive(-850, 800, 5);
+    intake.move(-127);
+    RwingsP.set(false);
+    LwingsP.set(false);
+
+    drive(600, 300);
     turn_to(0);
     drive(1000, 1000);
-    turn_to(55);
-    
+    turn_to(52);
     drive(2220);
-    delay(100);
-    turn_to(180);
-    drive(200);
-    intake.move(127);
     
-    turn_to(-75);
-    intake.move(-127);
-    drive(600);
-    delay(100);
-    drive(-600);
     turn_to(180);
-    intake.move(127);
-    drive(-100);
+    intake.move(100);
+    drive(-100, 200);
     turn_to(0);
+
     LwingsP.set(true);
     RwingsP.set(true);
-    drive(-900, 1500, 3);
-    drive(500);
-    turn_to(-90);
+    drive(-1550, 900, 3);
+    drive(500, 300);
+    turn_to(-90, 300);
     blockerP.set(true);
 }
 
 void rush_right_straight(){// trigger half cata 
     intake.move(-127);
-    drive(200, 400);
+    drive(200, 300);
     intake.move(0);
-    drive(-1600);
-    
-    turn_to(-45);
+    drive(-1600, 950);
+
+    turn_to(-37, 300);
     hangP.set(true);
-    drive(-685);
-    turn_to(-60);
-    drive(-100, 100);
-    hangP.set(false);
-    LwingsP.set(true);
-    turn_to(-90);
-    drive(-500);
-    LwingsP.set(false);
-    turn_to(90);
-    intake.move(127);
-    drive(100, 100);
-    turn_to(-90);
-    drive(-750, 1500, 5);
-    intake.move(-127);
+    delay(100);
+    drive(-1000, 900, 1, 75);
     
-    drive(600);
+    turn_to(-90, 700, 1, 80);
+    hangP.set(false);
+    RwingsP.set(true);
+    drive(-700, 200);
+    
+    RwingsP.set(false);
+    turn_to(90, 600);
+    
+    intake.move(127);
+    drive(-100, 200);
+    
+    turn_to(-70);
+    RwingsP.set(true);
+    LwingsP.set(true);
+    drive(-850, 800, 5);
+    intake.move(-127);
+    RwingsP.set(false);
+    LwingsP.set(false);
+
+    drive(600, 300);
     turn_to(0);
     drive(1000, 1000);
-    turn_to(55);
-    
+    turn_to(52);
     drive(2220);
-    delay(100);
-    turn_to(180);
-    drive(200);
-    intake.move(127);
     
-    turn_to(-75);
-    intake.move(-127);
-    drive(600);
-    delay(100);
-    drive(-600);
     turn_to(180);
-    intake.move(127);
-    drive(-100);
+    intake.move(100);
+    drive(-100, 200);
     turn_to(0);
+
     LwingsP.set(true);
     RwingsP.set(true);
-    drive(-900, 1500, 3);
-    drive(500);
-    turn_to(90);
+    drive(-1550, 900, 3);
+    drive(500, 300);
+    turn_to(90, 300);
 
 }
-void right_safe_block(){ //edting this one
+void right_safe_block(){ // trigger half cata -- edting this one #1 safe
     
     hangP.set(true);
     drive(-685);
@@ -236,7 +232,7 @@ void right_safe_block(){ //edting this one
 
 }
 
-void right_safe_bar(){
+void right_safe_bar(){ // trigger half cata -- edting this one #2 safe
     hangP.set(true);
     drive(-685);
     turn_to(-60);
@@ -396,6 +392,68 @@ void old_right(){
 
 void blank(){}
 
+void six_ball(){
+    intake.move(-127);
+    drive(200, 300);
+    intake.move(0);
+    drive(-1600, 950);
+    
+    turn_to(-37, 300);
+    hangP.set(true);
+    delay(100);
+    drive(-1000, 900, 1, 75);
+    
+    turn_to(-90, 700, 1, 80);
+    hangP.set(false);
+    RwingsP.set(true);
+    drive(-700, 300);
+    
+    RwingsP.set(false);
+    turn_to(90);
+    
+    intake.move(127);
+    drive(-100, 100);
+    
+    
+    turn_to(-70);
+    
+    RwingsP.set(true);
+    LwingsP.set(true);
+    drive(-1200, 1500, 5);
+    intake.move(-127);
+    RwingsP.set(false);
+    LwingsP.set(false);
+    
+    
+    drive(600);
+    turn_to(0);
+    drive(1000, 1000);
+    turn_to(52);
+    
+    drive(2220);
+    
+    turn_to(180);
+    intake.move(100);
+    delay(200);
+    
+    
+    
+    turn_to(-70);
+    intake.move(-127);
+    drive(600);
+    delay(50);
+    drive(-600, 500);
+    turn_to(180, 300);
+    intake.move(100);
+    drive(-350, 300);
+    turn_to(0);
+    LwingsP.set(true);
+    RwingsP.set(true);
+    drive(-1500, 1000, 3);
+    drive(500);
+    turn_to(-90);
+    blockerP.set(true);
+}
 
 void test_mode()
 {
